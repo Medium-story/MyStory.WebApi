7e046e64d87ac1cef4c6fd81dfaa569af6711cb1
MuhammadumarMM <botirovmuhammadumar33.gmail.com>
2023-12-25 21:33:00 +0500

    ok 200

--- MyStory.DTOs/Dtos/FollowDtos/UpdateFollowDto.cs
+++ MyStory.DTOs/Dtos/FollowDtos/UpdateFollowDto.cs
@@ -1,8 +1,8 @@
 ﻿using MyStory.DTOs.Dtos;
 
-namespace MyStory.DTOs.FollwDtos;
+namespace MyStory.DTOs.Dtos.FollowDtos;
 
-public class FollowDto : BaseDto
+public class UpdateFollowDto : BaseDto
 {
     public string FollowerUserId { get; set; } = string.Empty;
     public string FollowingUserId { get; set; } = string.Empty;
