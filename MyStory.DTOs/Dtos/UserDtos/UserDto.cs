54a77493bc0d7d3d2ca5ddacf21fb7b02b0aabb1
sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-12 15:04:17 +0500

    Hammsi ishladi hozircha user get all ham article get all ham

--- MyStory.DTOs/Dtos/UserDtos/UserDto.cs
+++ MyStory.DTOs/Dtos/UserDtos/UserDto.cs
@@ -6,8 +6,9 @@ using MyStory.DTOs.Dtos.ReplyDtos;
 
 namespace MyStory.DTOs.Dtos.UserDtos;
 
-public class UserDto : BaseDto
+public class UserDto
 {
+    public string Id { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
     public string AvatarUrl { get; set; } = string.Empty;
